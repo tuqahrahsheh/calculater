3ebe4e8cce06879f0d9c5fe0be5f758e111c48cb
tuqahrahsheh <tuqahrahsheh00@gmail.com>
2024-07-04 09:12:25 +0300

    Add project files.

---
+++ calculater/calculater.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="calculater.aspx.cs" Inherits="calculater.calculater" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="+" BackColor="#FF66FF" OnClick="Button1_Click" Width="112px" />
+            <asp:Button ID="Button2" runat="server" Text="-" BackColor="#33CCFF" OnClick="Button2_Click" Width="107px" />
+            <asp:Button ID="Button3" runat="server" Text="*" BackColor="#FF3300" OnClick="Button3_Click" Width="115px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ calculater/calculater.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace calculater
+{
+    public partial class calculater : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int number1 = int.Parse(TextBox1.Text);
+            int number2 = int.Parse(TextBox2.Text);
+
+            int result = number1 + number2;
+            Label1.Text = "Result : " + result.ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int number1 = int.Parse(TextBox1.Text);
+            int number2 = int.Parse(TextBox2.Text);
+
+            int result = number1 - number2;
+            Label1.Text = "Result : " + result.ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            int number1 = int.Parse(TextBox1.Text);
+            int number2 = int.Parse(TextBox2.Text);
+
+            int result = number1 * number2;
+            Label1.Text = "Result : " + result.ToString();
+        }
+    }
+}
